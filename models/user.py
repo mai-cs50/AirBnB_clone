a226a1015f03c4045219847d43477628aca6660b
mai-cs50 <maimohamed.1992@yahoo.com>
2023-12-07 10:12:43 -0800

    s

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' '''
+from models.base_model import BaseModel
+import json
+
+class User(BaseModel):
+    ''' '''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
