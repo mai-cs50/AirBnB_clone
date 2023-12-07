f2906d4315ca04dc7711d9c81350eaf0ff7f9c22
mai-cs50 <maimohamed.1992@yahoo.com>
2023-12-07 12:04:12 -0800

    s

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ import cmd
 import re
 from shlex import split
 from models import storage
-from models.base_models import BaseModel
+from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
