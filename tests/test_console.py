bc35f735019b68ea36347bc61294c489ff66cf32
mai-cs50 <maimohamed.1992@yahoo.com>
2023-12-09 12:02:38 -0800

    s

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''tested with unit tests'''
+import unittest
+import console
+import os
+import pep8
+import sys
+import json
+from models import storage
+from console import HBNBCommand
+from unittest.mock import patch
+from models.engine.file_storage import FileStorage
+
+class TestHBNBCommand_prompting(unittest.TestCase):
+    ''' '''
