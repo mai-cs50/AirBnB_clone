8d8b99735456532bf710ee3e6a7428a5ad34564f
mai-cs50 <maimohamed.1992@yahoo.com>
2023-12-09 12:22:48 -0800

    s

--- tests/test_console.py
+++ tests/test_console.py
@@ -14,3 +14,21 @@ from models.engine.file_storage import FileStorage
 
 class TestHBNBCommand_prompting(unittest.TestCase):
     ''' '''
+    @classmethod
+    def setupcalss(self):
+        ''' '''
+        self.typing = console.HBNBCommand()
+
+    @classmethod
+    def teardownclass(self):
+        ''' '''
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_pep8_console(self):
+        ''' '''
+        style = pep8.StyleGuide(quiet=False)
+        errors = 0
+        file = (["console.py"])
